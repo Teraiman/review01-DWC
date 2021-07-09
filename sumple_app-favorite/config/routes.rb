5689cf156a8041ceaf6cd1f3eaf951cf306f1d3f
Teraiman <82441416+Teraiman@users.noreply.github.com>
2021-07-09 19:03:27 +0900

    Delete sumple_app-favorite directory

--- sumple_app-favorite/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :posts
-  root 'home#index'
-  post 'like/:id' => 'likes#create', as: 'create_like'
-  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
-end
